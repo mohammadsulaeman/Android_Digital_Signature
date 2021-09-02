f63ceffabbc1e447633a49d195c1a8e5317b4687
mohammadsulaeman <sulaemanm991@gmail.com>
2021-09-02 08:37:25 +0700

    first commit

---
+++ app/src/main/java/com/example/signature/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.signature;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent main = new Intent(SplashActivity.this,MainActivity.class);
+                main.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(main);
+            }
+        },4000);
+    }
+}
